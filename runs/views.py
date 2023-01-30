1469fbf68cedc01c779c4c8f91132100da976eae
Craig West <iwswordpress@gmail.com>
2023-01-30 08:56:05 +0000

    add pagination for runs

--- runs/views.py
+++ runs/views.py
@@ -1,5 +1,5 @@
-
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 
 from .models import Run
@@ -7,8 +7,17 @@ from .models import Run
 
 def runs(request):
     runs = Run.objects.all()
-    context = {"runs": runs}
+
+    paginator = Paginator(runs, 5)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    print(page_obj)
+    runs = paginator.page(page_number)
+    context = {"runs": runs, "page_obj": page_obj}
+
     return render(request, "runs/runs.html", context)
+
 
 def createRunCSV(request):
     context = {}
@@ -16,11 +25,9 @@ def createRunCSV(request):
 
 
 def run(request, pk):
-    print('pk',pk)
+    print("pk", pk)
     run = Run.objects.get(run_id=pk)
     print(run)
-    context = {'run': run}
- 
-    return render(request, 'runs/run-detail.html', context)
+    context = {"run": run}
 
-
+    return render(request, "runs/run-detail.html", context)
