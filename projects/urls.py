5d33d651c7362fd3dda8fdf279c0f9f91f5b9579
Craig West <iwswordpress@gmail.com>
2023-01-30 05:05:43 +0000

    edits

--- projects/urls.py
+++ projects/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
-from . import views_projects
+from . import views
 app_name = 'projects'
 urlpatterns = [
-    path('', views_projects.projects, name="projects"),
-    path('project/<str:pk>/', views_projects.project, name="project"),
-    path('create-project/', views_projects.createProject, name="create-project"),
+    path('', views.projects, name="projects"),
+    path('project/<str:pk>/', views.project, name="project"),
+    path('create-project/', views.createProject, name="create-project"),
     path('update-project/<str:pk>/',
-         views_projects.updateProject, name="update-project"),
+         views.updateProject, name="update-project"),
     path('project-delete/<str:pk>/',
-         views_projects.deleteProject, name="project-delete"),
+         views.deleteProject, name="project-delete"),
     path('project-delete-confirm/<str:pk>/',
-         views_projects.deleteProjectConfirm, name="project-delete-confirm"),
+         views.deleteProjectConfirm, name="project-delete-confirm"),
 ]
