aa308e15c6649d0a769ec6ea834febe3731365bd
Craig West <iwswordpress@gmail.com>
2023-02-18 14:04:21 +0000

    add runs

--- runs/views.py
+++ runs/views.py
@@ -17,7 +17,7 @@ def runs(request):
     if not page_number:
         page_number = 1
 
-    paginator = Paginator(runs, 10)
+    paginator = Paginator(runs, 3)
 
     page_obj = paginator.get_page(page_number)
     runs = paginator.page(page_number)
