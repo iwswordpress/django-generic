9c3e2cddd542cb22d0193cda27bc7c9462f3cc14
Craig West <iwswordpress@gmail.com>
2023-02-25 21:20:55 +0000

    edit

--- csvs/models.py
+++ csvs/models.py
@@ -59,3 +59,31 @@ class Test(models.Model):
     run_id = models.CharField(max_length=255)
     project_id = models.PositiveIntegerField(default=0)
     accuracy = models.FloatField(default=0.0)
+
+
+class PycaretRun(models.Model):
+
+    run_id = models.CharField(
+        max_length=255,
+        unique=True,
+        primary_key=True,
+        editable=False,
+        default=uuid.uuid4,
+    )
+    uploaded_file = models.ForeignKey(UploadedFile, on_delete=models.PROTECT, null=True)
+    project_id = models.PositiveIntegerField(default=1)
+    data_scientist_id = models.PositiveIntegerField(default=1)
+    mlr_dataset = models.CharField(max_length=255, default="datased-used")
+    model_used = models.CharField(max_length=1000, default="model-parameters")
+    holdout_acc = models.FloatField(default=0.0)
+    metrics_dict = models.CharField(max_length=1000, default="{'example_field':'value}")
+    accuracy = models.FloatField(default=0.0)
+    roc_auc = models.FloatField(default=0.0)
+    recall = models.FloatField(default=0.0)
+    precision = models.FloatField(default=0.0)
+    f1 = models.FloatField(default=0.0)
+    kappa = models.FloatField(default=0.0)
+    mcc = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.run_id
