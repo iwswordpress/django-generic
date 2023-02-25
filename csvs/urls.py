f8b8ea590d0ed27dc7889eff92cd1798c5b91e76
Craig West <iwswordpress@gmail.com>
2023-02-25 19:10:15 +0000

    set up pandas page

--- csvs/urls.py
+++ csvs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from . import views_csv
+from . import views_pd
 
 app_name = "csvs"
 
@@ -9,4 +10,5 @@ urlpatterns = [
     # path("upload-csv/", views_csv.uploadCSV, name="upload-csv"),
     path("upload-file-form/", views_csv.uploadCSVFile, name="upload-file-form"),
     path("run/<str:pk>/", views.run, name="run"),
+    path("pandas/", views_pd.pandas_home, name="pandas"),
 ]
---
+++ csvs/views_pd.py
@@ -0,0 +1,23 @@
+import os
+
+import pandas as pd
+
+from django.conf import settings
+from django.urls import path
+from django.shortcuts import render
+
+from .models import UploadedFile
+
+
+def pandas_home(request):
+    folder = "uploads"
+    file_path = os.path.join(folder, "data", "pycaret_results.csv")
+    # Join various path components
+    print(file_path)
+    try:
+        df = pd.read_csv(file_path)
+        print(df)
+    except:
+        pass
+    context = {"info": file_path, "df": df}
+    return render(request, "csvs/pandas.html", context)
